fcede17d35856d224538818d55bd4457987f455a
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2023-01-26 18:36:21 +0100

    chore: add update.sh script

---
+++ update.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -euo pipefail
+
+file="Formula/${1}.rb"
+new_version="${2}"
+
+version_pattern="v[0-9]*\.[0-9]*\.[0-9]*"
+old_version="$(grep "version \"${version_pattern}\"" "${file}" | grep -o "${version_pattern}")"
+
+formula="$(cat "${file}")"
+echo "${formula//${old_version}/${new_version}}" > "${file}"
+
+url_pattern="https.*\.gz"
+new_url=$(grep "url \"${url_pattern}\"" "${file}" | grep -o "${url_pattern}")
+
+for url in ${new_url}
+do
+    base_url="$(echo "${url}" | grep -o "https.*${version_pattern}/")"
+    tar="${url//${base_url}/}"
+    old_base_url="${base_url//${new_version}/${old_version}}"
+
+    curl -Lo "${tar}" "${old_base_url}${tar}"
+    old_sha="$(shasum -a 256 "${tar}")"
+    old_sha="${old_sha//  ${tar}/}"
+    rm "${tar}"
+
+    curl -Lo "${tar}" "${base_url}${tar}"
+    new_sha="$(shasum -a 256 "${tar}")"
+    new_sha="${new_sha//  ${tar}/}"
+    rm "${tar}"
+
+    formula="$(cat "${file}")"
+    echo "${formula//${old_sha}/${new_sha}}" > "${file}"
+done
