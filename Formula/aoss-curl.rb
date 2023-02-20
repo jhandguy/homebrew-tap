b06a8b097f9c119d7bc7f77ae09cef49653f1cb1
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2023-02-20 19:29:12 +0100

    chore: update aoss-curl formula

--- Formula/aoss-curl.rb
+++ Formula/aoss-curl.rb
@@ -1,26 +1,26 @@
 class AossCurl < Formula
     desc "Request to Amazon OpenSearch Service with SigV4 🔏"
     homepage "https://github.com/jhandguy/aoss-curl"
-    version "v0.1.19"
+    version "v0.1.20"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.19/x86_64-apple-darwin.gz"
-        sha256 "d1d10dc6967e4714b457afbbb0b0ee7c9810f9e5ab7dacde08af7c9458c25705"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.20/x86_64-apple-darwin.gz"
+        sha256 "3eb87d213bc670f657730a14241a79606f1a4b22468b7c79080510a7c04d2e6c"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.19/aarch64-apple-darwin.gz"
-        sha256 "52b1d3c09239f3264c28d794eca17c132d59c9eaf9b96a72b3f09b4f91c0e0d5"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.20/aarch64-apple-darwin.gz"
+        sha256 "43bf14debaa85f943f5478205ea6dc5ad31ecd441adaee0bd131969676d696a3"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.19/x86_64-unknown-linux-gnu.gz"
-        sha256 "9db918ccf0a144db690a474def225aa0cf289e3f281fefee4fec64d16b0bd0c4"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.20/x86_64-unknown-linux-gnu.gz"
+        sha256 "3f71bde15ea9e601067a4991b08094e85970fcc98b6aaefeca0503f04782bd4f"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.19/aarch64-unknown-linux-gnu.gz"
-        sha256 "84edfb0eb8dd66c41a2a1c4afdd61ee2405bfaed7865ac212b1b2872e0456392"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v0.1.20/aarch64-unknown-linux-gnu.gz"
+        sha256 "14ce91ae1e327a1be390d13296a40a9e5d3f12bb489b18a80a8256c3eb7752ca"
     end
 
     def install
