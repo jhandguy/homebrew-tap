fdddd5ca1a71caf891b8fd6a39887078caf80d95
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2023-03-06 22:20:16 +0100

    chore: update aws-mfa formula

--- Formula/aws-mfa.rb
+++ Formula/aws-mfa.rb
@@ -1,26 +1,26 @@
 class AwsMfa < Formula
     desc "Authenticate to AWS with MFA 🔐"
     homepage "https://github.com/jhandguy/aws-mfa"
-    version "v0.2.15"
+    version "v0.2.16"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.15/x86_64-apple-darwin.gz"
-        sha256 "c3e596dab4bd0f397ef06ee2f91095656f0c22db24740caa4a68860af87a7932"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.16/x86_64-apple-darwin.gz"
+        sha256 "3ec29afa1862c45820b71ae80acaf7eb557be56d5ba3c5fc948ec79390ee45e2"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.15/aarch64-apple-darwin.gz"
-        sha256 "67a1680c42781bf28746b4dddbacd0a98cca8a88277a2d532059a8ef68912bcc"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.16/aarch64-apple-darwin.gz"
+        sha256 "625a8e7c7df3271382526fb392de5496a7afdf1f50c92f20aa055a338ca5148f"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.15/x86_64-unknown-linux-gnu.gz"
-        sha256 "4ef3e75924b359221cbe51dc1e94a554778df200e8a9d2f5a132196f7372eea3"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.16/x86_64-unknown-linux-gnu.gz"
+        sha256 "27b2f6a2df339297c8d73eae1fdd85ceac43df3b0b50063f64241b984f3ffcfb"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.15/aarch64-unknown-linux-gnu.gz"
-        sha256 "cb7fc3e1cdf8b765952d28370147dce345f5dd7d0799da41a6222736251bda37"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.2.16/aarch64-unknown-linux-gnu.gz"
+        sha256 "de9d8e2ddf5cd4017b2c22e8cacc9907040c54028d7467161eada738d4b537f2"
     end
 
     def install
@@ -28,6 +28,6 @@ class AwsMfa < Formula
     end
 
     test do
-        system "#{bin}/aws-mfa --help"
+        system "#{bin}/aws-mfa --version"
     end
 end
