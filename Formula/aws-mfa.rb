04c5fcbd56d4af0eac4cf9da93ba5c1f1c59a10e
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2023-04-15 18:24:09 +0200

    chore: update aws-mfa formula

--- Formula/aws-mfa.rb
+++ Formula/aws-mfa.rb
@@ -1,26 +1,26 @@
 class AwsMfa < Formula
     desc "Authenticate to AWS with MFA 🔐"
     homepage "https://github.com/jhandguy/aws-mfa"
-    version "v0.3.0"
+    version "v0.3.1"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.0/x86_64-apple-darwin.gz"
-        sha256 "71619b5f307e065c720c92b8e5f7bbba0f6bdca9a417fc458287adf7c9514c4c"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.1/x86_64-apple-darwin.gz"
+        sha256 "e6e04f6a9c161d3b0bb21e5a8539a2530a55ab8868a9acade3385340b5beb2c2"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.0/aarch64-apple-darwin.gz"
-        sha256 "d13b52b3e3464ddb92884cf15762bba0ad7f9d40d37e8cee1d738b4cc4bf0e6d"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.1/aarch64-apple-darwin.gz"
+        sha256 "439c5146fdfb7af6927d2a389007043a4e6953eabcbe86c697cfae204e6b370f"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.0/x86_64-unknown-linux-gnu.gz"
-        sha256 "01c93283122bb207de6da22a8cee99dbfb69d15b57b1a1a4d938fe73ca6680cf"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.1/x86_64-unknown-linux-gnu.gz"
+        sha256 "6cf60746467a3776de71e3629bd6191cebc4abc6d6fcdd5f72340b7f0b94a3d1"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.0/aarch64-unknown-linux-gnu.gz"
-        sha256 "93a546bbdd0ec706762bef3693129ee02e803f2d6298ed9de6d7ee0aa6aa2a97"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.1/aarch64-unknown-linux-gnu.gz"
+        sha256 "0c3c831ebb1722d7112694d531e64d93eeb75a3fe172394250bf4a58e437ef70"
     end
 
     def install
