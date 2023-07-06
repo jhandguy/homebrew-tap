cc905374090a9daa81976b8eb9e5b56e2d16944e
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2023-07-06 10:58:35 +0200

    chore: update aws-mfa formula

--- Formula/aws-mfa.rb
+++ Formula/aws-mfa.rb
@@ -1,26 +1,26 @@
 class AwsMfa < Formula
     desc "Authenticate to AWS with MFA 🔐"
     homepage "https://github.com/jhandguy/aws-mfa"
-    version "v0.3.3"
+    version "v0.3.4"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.3/x86_64-apple-darwin.gz"
-        sha256 "978e70c6a3aee753bc357c26141e6678d02403973eb6f3d7d0d007367ec55dfb"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.4/x86_64-apple-darwin.gz"
+        sha256 "1236924a84d54ca04532a60b10448039c64d919605442a4a233154054f052a45"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.3/aarch64-apple-darwin.gz"
-        sha256 "7fb76077777185c866aabd5b32e852f75cee6e0edbd1390f10f08806f328d386"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.4/aarch64-apple-darwin.gz"
+        sha256 "656505d0de6192b92489c24ca3089cf7e78bae02f9989ad7831588e8d3c9e33b"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.3/x86_64-unknown-linux-gnu.gz"
-        sha256 "6ad9322bd74bcc76c89c7414141cfb3096860ddfc4669f94df49c437d0d1692c"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.4/x86_64-unknown-linux-gnu.gz"
+        sha256 "324ebac531897c06dc507ea8fd5e69f477da6af2ae770b4e811c43b7d87734af"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.3/aarch64-unknown-linux-gnu.gz"
-        sha256 "dc9d80c4645dc1cfa24c1d8585307e718d5be2f6b6350170f6db8b4531625896"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v0.3.4/aarch64-unknown-linux-gnu.gz"
+        sha256 "3fee4fc16818acbd42cfae10df9fc605eefc8fb292316fbfd9280091e9714d5b"
     end
 
     def install
