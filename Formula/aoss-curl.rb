a25d496ad877128494a2978ecc634e949e80e0ac
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2023-09-10 16:58:37 +0200

    chore(release): aoss-curl v1.0.1

--- Formula/aoss-curl.rb
+++ Formula/aoss-curl.rb
@@ -1,27 +1,27 @@
 class AossCurl < Formula
     desc "Request to Amazon OpenSearch Service with SigV4 🔏"
     homepage "https://github.com/jhandguy/aoss-curl"
-    version "v1.0.0"
+    version "v1.0.1"
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.0/x86_64-apple-darwin.gz"
-        sha256 "776ada6d4d107405baf2b99de52d195e6d00683f0e740403cd9d1382a5ef3741"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.1/x86_64-apple-darwin.gz"
+        sha256 "f4e1ee5339bcde511fc1a09e9bc4f4993485752c5a99163c301b540b4a190693"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.0/aarch64-apple-darwin.gz"
-        sha256 "730a878eb5aacea9ff8ca37c16b2ac91c6b59377443fbb61bb183d80a21ce232"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.1/aarch64-apple-darwin.gz"
+        sha256 "4acbd116e6742b1dd09caf21cc394df4c8bb0e32ab5d3f09132c62f482bbc0c1"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.0/x86_64-unknown-linux-gnu.gz"
-        sha256 "d8b95a6877bbc8018ee5daeb6b00509e97f191a3378897c5c61859693a84d376"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.1/x86_64-unknown-linux-gnu.gz"
+        sha256 "e7b5630801b7b19ea23f069b0f7ca20e8646777885b06313b4eb8fd117e33c2f"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.0/aarch64-unknown-linux-gnu.gz"
-        sha256 "39651b48530b9708ee1c6c66f7dc22abb5b0d42598c5c76d46addb1dbd2a6517"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.1/aarch64-unknown-linux-gnu.gz"
+        sha256 "c08e7543019d6b7c2826266d1e3e413d918e6429caedba8aa508dde63623dcb0"
     end
 
     def install
