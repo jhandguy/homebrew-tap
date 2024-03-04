e3dc23a5c39e42411446425c87324aae025e6810
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2024-03-04 21:54:41 +0100

    chore(release): obsidian-vault v0.0.4

--- README.md
+++ README.md
@@ -10,7 +10,8 @@ brew tap jhandguy/tap
 
 ## Formulae
 
-| Formula                                            | Install Command                       |
-|----------------------------------------------------|---------------------------------------|
-| [aws-mfa](https://github.com/jhandguy/aws-mfa)     | `brew install jhandguy/tap/aws-mfa`   |
-| [aoss-curl](https://github.com/jhandguy/aoss-curl) | `brew install jhandguy/tap/aoss-curl` |
+| Formula                                                      | Install Command                            |
+|--------------------------------------------------------------|--------------------------------------------|
+| [aws-mfa](https://github.com/jhandguy/aws-mfa)               | `brew install jhandguy/tap/aws-mfa`        |
+| [aoss-curl](https://github.com/jhandguy/aoss-curl)           | `brew install jhandguy/tap/aoss-curl`      |
+| [obsidian-vault](https://github.com/jhandguy/obsidian-vault) | `brew install jhandguy/tap/obsidian-vault` |
--- formula/obsidian-vault.rb
+++ formula/obsidian-vault.rb
@@ -1,22 +1,27 @@
 class ObsidianVault < Formula
     desc "obsidian-vault is a CLI to backup your Obsidian notes in GitHub using AES-GCM-256 authenticated encryption."
     homepage "https://github.com/jhandguy/obsidian-vault"
-    version "v0.0.3"
+    version "v0.0.4"
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.3/ov_darwin_amd64.tar.gz"
-        sha256 "cc6eb5fd753d7112ad3bcfd16fe43ea47e396ed94f81ee838176906666bae271"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.4/ov_darwin_amd64.tar.gz"
+        sha256 "e93b65d00425ec6a82816579d254888abe9d9bc43c7482d9194a17137e92bfd7"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.3/ov_darwin_arm64.tar.gz"
-        sha256 "6ddddc302219b3770cd06c5a6dd47d4fa417b239393cc4775fc6c0e0237273bb"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.4/ov_darwin_arm64.tar.gz"
+        sha256 "a482efe4c6f1bfbf162cde6c6cc94357936b1c7d5577dff8169bc6e7fe7fb91f"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.3/ov_linux_amd64.tar.gz"
-        sha256 "e2456e182440dde61c67fd7cdc537fe0db3e828e09a0234802f9dee77b3e3de6"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.4/ov_linux_amd64.tar.gz"
+        sha256 "2f5fcfe25a22ade0d67eefcc93d89d1b19bab754a608c16155bce9ee39c7ff0c"
+    end
+
+    if OS.linux? && Hardware::CPU.arm?
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.4/ov_linux_arm64.tar.gz"
+        sha256 "f9d06340d250ac982aee25e95db4e63af83233533430a89040450d01bf4a5dbc"
     end
 
     def install
