86f8ef906a2fc81c5e2917a024d484414e3098a6
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2024-04-22 14:16:09 +0200

    chore(release): obsidian-vault v0.0.6

--- formula/obsidian-vault.rb
+++ formula/obsidian-vault.rb
@@ -1,27 +1,27 @@
 class ObsidianVault < Formula
     desc "obsidian-vault is a CLI to backup your Obsidian notes in GitHub using AES-GCM-256 authenticated encryption."
     homepage "https://github.com/jhandguy/obsidian-vault"
-    version "v0.0.5"
+    version "v0.0.6"
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.5/ov_darwin_amd64.tar.gz"
-        sha256 "f390866b5e8082968283c76f2dd1477a238648a2110c80e32566ad75cb92a3c3"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.6/ov_darwin_amd64.tar.gz"
+        sha256 "bc8ae4a2e693087c800051f6c189c26db4425d695b3e1ba946f7a694005efe0f"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.5/ov_darwin_arm64.tar.gz"
-        sha256 "1341615d0c4073ab3a190763aa79730c824b634fab87b0d41eecac578cdd7a82"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.6/ov_darwin_arm64.tar.gz"
+        sha256 "ff63855d9b11dd37a05d8f3a98f247fcc774c9c7be6b5b9ba6c533487e420ddc"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.5/ov_linux_amd64.tar.gz"
-        sha256 "92bc9414d73bd0c5e04c4bf907c31fc29ab91f43593ba47fec3bb782fe736622"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.6/ov_linux_amd64.tar.gz"
+        sha256 "0af503ef5d264e987b2c07cce7b0cd38dfd608902bfb8475602b6f0d1d5abed2"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.5/ov_linux_arm64.tar.gz"
-        sha256 "262a5ca568ce5ad01b529cceba4ac58bafa332b938ddb092bb07ccf40bf6bcbb"
+        url "https://github.com/jhandguy/obsidian-vault/releases/download/v0.0.6/ov_linux_arm64.tar.gz"
+        sha256 "af5b7d95fad8c3594545a71b227b0c13b728ca634250bd372fb7f416c58ac4c9"
     end
 
     def install
