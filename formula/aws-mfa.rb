fab89283378bb5af75d7020746db717fbacaca66
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2024-10-19 22:36:59 +0200

    chore(release): aws-mfa v1.0.18

--- formula/aws-mfa.rb
+++ formula/aws-mfa.rb
@@ -1,27 +1,27 @@
 class AwsMfa < Formula
     desc "Authenticate to AWS with MFA 🔐"
     homepage "https://github.com/jhandguy/aws-mfa"
-    version "v1.0.17"
+    version "v1.0.18"
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.17/x86_64-apple-darwin.gz"
-        sha256 "9bd3916e9d4a09d6f83a46dc91cbfa62c0907cbe8992c4e48ff5fd386d0dfc98"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.18/x86_64-apple-darwin.gz"
+        sha256 "bc30e77027e6a0ad6da03e1d2440e8ad2b0db66765b736ea3e626119e1f7acb2"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.17/aarch64-apple-darwin.gz"
-        sha256 "f7b431334c3d46284d552ff99a830ebd4650b13b61114f460f085f0293baaf0d"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.18/aarch64-apple-darwin.gz"
+        sha256 "28d9f10703bcc35a6a3ef90d2b6deacb3675b9a7a6904f9462052a821f0825a6"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.17/x86_64-unknown-linux-gnu.gz"
-        sha256 "b46e93985371e200a3b63eab4f7c3f98282931c77a62aaf9b2625c2e4d594f24"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.18/x86_64-unknown-linux-gnu.gz"
+        sha256 "be8cf1a0437213eaa080166bf4a4daec0a6327111229e98041c27e88a38fde4b"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.17/aarch64-unknown-linux-gnu.gz"
-        sha256 "ccbe788d8db7c87826abc08adb533eb1666488378cef58bf134e635cb4e71913"
+        url "https://github.com/jhandguy/aws-mfa/releases/download/v1.0.18/aarch64-unknown-linux-gnu.gz"
+        sha256 "bc42b3d205e74e127c32038e98f25bf0382012d3a469360a46a91bff3d1dbf7b"
     end
 
     def install
