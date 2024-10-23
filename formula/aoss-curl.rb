492433ba8ea868e9d322bd64d030c78bf8c409d9
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2024-10-23 22:01:35 +0200

    chore(release): aoss-curl v1.0.19

--- formula/aoss-curl.rb
+++ formula/aoss-curl.rb
@@ -1,27 +1,27 @@
 class AossCurl < Formula
     desc "Request to Amazon OpenSearch Service with SigV4 🔏"
     homepage "https://github.com/jhandguy/aoss-curl"
-    version "v1.0.18"
+    version "v1.0.19"
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.18/x86_64-apple-darwin.gz"
-        sha256 "6d042fafdc8dc755965f218f1c0f0bb95b8e775dd8be645dcf9cb3019554df62"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.19/x86_64-apple-darwin.gz"
+        sha256 "981fd6c9d0991214eb8a2feb5d9ba75b72913d4e3430a6f73698114af462b6ae"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.18/aarch64-apple-darwin.gz"
-        sha256 "207dc87428351f3d2ceaa88eca04f9aea9ac4724f6ab42713d5f7b3fadea3517"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.19/aarch64-apple-darwin.gz"
+        sha256 "e7ad32f444e85f29ca92c1cab0f032636497cb073c3e81490b3737d0eedef27d"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.18/x86_64-unknown-linux-gnu.gz"
-        sha256 "2ad483e6a26470be57d77d0afc535a48f4bfb2fed951bcd3f6377e2cf74ab6ba"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.19/x86_64-unknown-linux-gnu.gz"
+        sha256 "fa1fc69f8cf849280cf2d21be57164c2c41b98de2b9c81daa7a6a1cfd660dd54"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.18/aarch64-unknown-linux-gnu.gz"
-        sha256 "a2dd07d6c06a49b1917e8f8075a7a311db0d5294cd1b643dc4aa3e8d783b507f"
+        url "https://github.com/jhandguy/aoss-curl/releases/download/v1.0.19/aarch64-unknown-linux-gnu.gz"
+        sha256 "f18d7c2e170c6b61007d53a63bbe03ab881274ce9b1600038e50d94c13927a40"
     end
 
     def install
