496c17a4814eba4e0c2d59ff57b3144bdbf67f7c
Jean Mainguy <9969006+jhandguy@users.noreply.github.com>
2025-12-25 20:02:23 +0100

    chore(release): jira-prompt v1.0.2

--- formula/jira-prompt.rb
+++ formula/jira-prompt.rb
@@ -1,27 +1,27 @@
 class JiraPrompt < Formula
     desc "jira-prompt is a CLI to prompt Ollama using data from Jira issues."
     homepage "https://github.com/jhandguy/jira-prompt"
-    version "v1.0.1"
+    version "v1.0.2"
     license "MIT"
 
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.1/jp_darwin_amd64.tar.gz"
-        sha256 "d57877ef04f5c9493d8398e991fc37bcc0c023598774b20d006cf045d64de60b"
+        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.2/jp_darwin_amd64.tar.gz"
+        sha256 "c94aab5ef5d4e579dfb068a190f52735b25ecbeba38b18698e0fb1e85cf32049"
     end
 
     if OS.mac? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.1/jp_darwin_arm64.tar.gz"
-        sha256 "83d654a8a592704d039548c3e08567bb1977c5bcc43d77f49c7d4b301c0b2b9b"
+        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.2/jp_darwin_arm64.tar.gz"
+        sha256 "1f55d2ad9de5af5fad46e881a840300a6921d32e2d05abeda3f2e3102f56be60"
     end
 
     if OS.linux? && Hardware::CPU.intel?
-        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.1/jp_linux_amd64.tar.gz"
-        sha256 "2dac2c1a934c3021fca6c48bd2c6f6b0fbd16e7882c91584f212c46766198b81"
+        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.2/jp_linux_amd64.tar.gz"
+        sha256 "fea7c51603070a83c92c58fad9234414ffd4c837e11cedc41fa9f33145601385"
     end
 
     if OS.linux? && Hardware::CPU.arm?
-        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.1/jp_linux_arm64.tar.gz"
-        sha256 "da9b1c642a506ec0ccb1970f6b8d052d007d4d21a1455c8427748052687a8f96"
+        url "https://github.com/jhandguy/jira-prompt/releases/download/v1.0.2/jp_linux_arm64.tar.gz"
+        sha256 "f6bd1a230cb362632d9f50dab89428fb7d94c987f3786562a28429e094f32bc1"
     end
 
     def install
